48b6fe5973b1b39481a8adc9a2a0bf468b203e55
Anne Homann <an.hmnn@gmail.com>
2014-04-29 22:55:23 +1000

    added admin column for admin user

--- db/schema.rb
+++ db/schema.rb
@@ -44,12 +44,12 @@ ActiveRecord::Schema.define(version: 20140429122225) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -57,7 +57,7 @@ ActiveRecord::Schema.define(version: 20140429122225) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "username"
-    t.boolean  "admin"
+    t.boolean  "admin",                  default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
