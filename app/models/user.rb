aa3bd4cb3a3c02d43a477b4faa5edf035ffa9853
markpereira <mark.pereira@xtra.co.nz>
2014-04-29 23:32:03 +1000

    user.rb modification

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,7 +24,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  attr_accessible :email, :username, :password, :password_confirmation
+  attr_accessible :email, :username, :password, :password_confirmation, :admin
   has_many :reservations
 
 
