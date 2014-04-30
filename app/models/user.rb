f3004fbd8883d99f3ab772b549c7d5d858d85e05
markpereira <mark.pereira@xtra.co.nz>
2014-04-30 14:03:19 +1000

    seed file update and bloack users from create flight and create plane

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,7 +24,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  attr_accessible :email, :username, :password, :password_confirmation, :admin
+  attr_accessible :email, :username, :password, :password_confirmation
   has_many :reservations
 
 
