90d378d1a773755dfe7e23ad6b46c49dc234ce17
Nizmox <nizmox@gmail.com>
2014-04-30 13:25:16 +1000

    working on reservations, unfinished

--- app/assets/javascripts/backbone/models/plane.js
+++ app/assets/javascripts/backbone/models/plane.js
@@ -2,16 +2,20 @@ BurningAirlines.Models.Plane = Backbone.Model.extend({
   urlRoot: '/planes',
 
   initialize: function () {
+    //create a seats collection and attach to this plane
     this.seats = new BurningAirlines.Collections.Seats();
+    //run the create seats function to populate the collection with all available seats
     this.createSeats();
     console.log('Seats: ', this.seats);
   },
 
   createSeats: function () {
 
+    var plane_id = this.get('id');
+
     for (var c = 1; c <= this.get('columns'); c++) {
       for (var r = 1; r <= this.get('rows'); r++) {
-        var seat = new BurningAirlines.Models.Seat({row: r, column: c});
+        var seat = new BurningAirlines.Models.Seat({plane_id: plane_id, row: r, column: c});
         this.seats.add(seat);
       }
     }
--- app/assets/javascripts/backbone/views/flightSeatView.js
+++ app/assets/javascripts/backbone/views/flightSeatView.js
@@ -11,6 +11,32 @@ BurningAirlines.Views.FlightSeatView = Backbone.View.extend({
 
     this.$el.html(this.template(this.model.toJSON()));
     return this;
+  },  
+
+  events: {
+    'click': 'reserve'
+  },
+
+  reserve: function () {
+    //issues:
+    //NO FLIGHT ID INCLUDED IN RESERVATION
+    //NO USER ID ASSOCIATED WITH THE SEAT
+    //NEED TO PREVENT DUPLICATE BOOKINGS OF THE SAME FLIGHT / SEAT
+
+    // debugger;
+
+    //get the plane_id, row number and column number for the clicked seat
+    var plane_id = this.model.get('plane_id');
+    var row = this.model.get('row');
+    var column = this.model.get('column');
+
+    console.log("you clicked seat: ", this.model.get('content'));
+
+    var reservation = new BurningAirlines.Models.Reservation({plane_id: plane_id, row_no: row, column_no: column});
+    reservation.save();
+    
+    console.log("reservation created: ", reservation);
   }
+
 });
 
